5c66ce7fa9c6e37718f36a0e87636c62c45a513d
Chetan Yadav <imchetanyadav@gmail.com>
2018-07-25 15:08:26 +0530

    Updated gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass', 
+    'gatsby-transformer-sharp',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/static/img/`,
+      },
+    },
+  ],
 }
